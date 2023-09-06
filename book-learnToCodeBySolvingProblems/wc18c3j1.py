f64c1d2291f58bb155ad94efbab30c097ab8d5c2
clipcrisp <corywstewart99@gmail.com>
2023-09-06 19:04:40 -0400

    Update last problem with better solution //

--- book-learnToCodeBySolvingProblems/wc18c3j1.py
+++ book-learnToCodeBySolvingProblems/wc18c3j1.py
@@ -2,7 +2,7 @@ paint = int(input())
 bottlecap_paint = int(input())
 cap_cost = int(input())
 
-painted_caps = int(paint / bottlecap_paint)
+painted_caps = paint // bottlecap_paint
 remainder_paint = paint % bottlecap_paint
 
 print((painted_caps * cap_cost) + remainder_paint)
