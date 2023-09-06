39cee7547a654ebd1a0dee55ab94513bbc0c8211
clipcrisp <corywstewart99@gmail.com>
2023-09-06 19:01:34 -0400

    Chapter 1 problems finished

---
+++ book-learnToCodeBySolvingProblems/wc18c3j1.py
@@ -0,0 +1,8 @@
+paint = int(input())
+bottlecap_paint = int(input())
+cap_cost = int(input())
+
+painted_caps = int(paint / bottlecap_paint)
+remainder_paint = paint % bottlecap_paint
+
+print((painted_caps * cap_cost) + remainder_paint)
